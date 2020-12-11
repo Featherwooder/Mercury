617d901d6c1b7cc959194ed60648c5317aa2d4bb
HUST Andy <709775207@qq.com>
2020-12-11 11:20:35 +0800

    asd

--- Mercury/miniprogram/pages/index1/index1.js
+++ Mercury/miniprogram/pages/index1/index1.js
@@ -5,14 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    avatarUrl:""
+  },
+  onLoad: function() {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onGetUserInfo: function(e) {
+    console.log(e)
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+      })        
+      if(this.data.logged){
+        wx.redirectTo({
+          url: '/pages/index/index',
+        })
+      }     
+    }
   },
 
   /**
