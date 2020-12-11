242de4f5354f55820fbe09a69c937c647f5a55a1
HustZHX <1009475425@qq.com>
2020-12-11 14:07:22 +0800

    123

---
+++ Mercury/miniprogram/pages/second/second.js
@@ -0,0 +1,183 @@
+// pages/second/second.js
+var util=require('../../util/util');//调用util中的获取时间函数
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hidden1:true,
+    hidden2:false,    
+    hidden3:true,
+    hidden4:false,
+    soup:"\n每个不曾起舞的日子，都是对生命的辜负！！"
+
+  },
+  turn: function() {
+    
+    // 1.创建动画实例(animation)
+    var animation1 = wx.createAnimation({
+      duration: 500,//动画持续时间
+      timingFunction: 'ease-in-out',//动画以低速开始
+    })
+    var animation2 = wx.createAnimation({
+      duration: 1000,//动画持续时间
+      timingFunction: 'ease-in-out',//动画以低速开始
+    })
+    animation1.rotateX(-90).scale(0,0).step()
+    animation2.rotate3d(-360).scale(1,1).step()
+
+    // 3.导出画
+    this.setData({
+      hidden4:false,
+      ani4:animation1.export(),
+      ani5:animation2.export(),
+    })
+  },
+  turnback: function() {
+    // 1.创建动画实例(animation)
+    var animation1 = wx.createAnimation({
+      duration: 800,//动画持续时间
+      timingFunction: 'ease-in-out',//动画以低速开始
+    })
+    var animation2 = wx.createAnimation({
+      duration: 800,//动画持续时间
+      timingFunction: 'ease-in-out',//动画以低速开始
+    })
+    animation1.rotate3d(180).scale(1,1).step()
+    animation2.rotate3d(180).scale(0,0).step()
+    // 3.导出画
+    this.setData({
+      ani4:animation1.export(),
+      ani5:animation2.export(),
+    })
+  },
+  close: function() {
+    
+    // 1.创建动画实例(animation)
+    var animation1 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-in',//动画以低速开始
+    })
+    var animation2 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-in',//动画以低速开始
+    })
+    var animation3 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-in',//动画以低速开始
+    })
+
+
+   
+    animation1.translate(0, 0).rotate(360).scale(0.1,0.1).step()
+    animation2.translate(0, 0).rotate(360).scale(0.1,0.1).step()
+    animation3.translate(0, 0).rotate(360).scale(0.1,0.1).step()
+
+  
+    // 3.导出动画
+    this.setData({
+      hidden2:false,
+      hidden1:true,
+      ani1:animation1.export(),
+      ani2:animation2.export(),
+      ani3:animation3.export()
+    })
+  },
+
+  go: function() {
+    
+    // 1.创建动画实例(animation)
+    var animation1 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-out',//动画以低速开始
+    })
+    var animation2 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-out',//动画以低速开始
+    })
+    var animation3 = wx.createAnimation({
+      duration: 200,//动画持续时间
+      timingFunction: 'ease-out',//动画以低速开始
+    })
+
+
+   
+    animation1.translate(0, -90).rotate3d(360).scale(1,1).step()
+    animation2.translate(-90, 0).rotate3d(360).scale(1,1).step()
+    animation3.translate(-63, -63).rotate3d(360).scale(1,1).step()
+
+  
+    // 3.导出动画
+    this.setData({
+      hidden2:true,
+      hidden1:false,
+      ani1:animation1.export(),
+      ani2:animation2.export(),
+      ani3:animation3.export()
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+
+
+    var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据
+    this.setData({
+      time: time
+    });
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
